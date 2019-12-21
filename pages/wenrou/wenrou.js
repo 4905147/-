ec52816bc8496f20b2967ac294721ef54b767154
4905147 <59112577+4905147@users.noreply.github.com>
2019-12-21 23:50:02 +0800

    1

---
+++ pages/wenrou/wenrou.js
@@ -0,0 +1,158 @@
+// pages/tuwei/tuwei.js
+//获取应用实例
+const app = getApp()
+Page({
+  // goBackHome回调 返回上一级页面
+  goBackHome: function () {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  // getQueryCallback回调, 返回数据
+  getQueryCallback: function (e) {
+  },
+  // 点击机器人回答里的链接跳转webview,需要开发者自己配置一个承载webview的页面,url字段对应的小程序页面需要开发者自己创建
+  // 开发者需要在小程序后台配置相应的域名
+  // 1.1.7版本开始支持
+  openWebview: function (e) {
+    let url = e.detail.weburl
+    wx.navigateTo({
+      url: `/pages/webviewPage/webviewPage?url=${url}`
+    })
+  },
+  // 点击机器人回答中的小程序，需要在开发者自己的小程序内做跳转
+  // 开发者需要在小程序配置中指定跳转小程序的appId
+  // 1.1.7版本开始支持
+  openMiniProgram(e) {
+    let { appid, pagepath } = e.detail
+    wx.navigateToMiniProgram({
+      appId: appid,
+      path: pagepath,
+      extraData: {
+      },
+      envVersion: '',
+      success(res) {
+        // 打开成功
+      }
+    })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  onLaunch: function () {
+    console.log(plugin3, "+++");
+    plugin3.init({
+      appid: "ZOo7mfv9JTsCwMCXchZ2jfnMcyH4or",
+      success: () => { },
+      fail: error => { },
+      guideList: ["您好"],
+      textToSpeech: true, //默认为ture打开状态
+      welcome: "请问有什么需要帮助？",
+      welcomeImage: 'http://inews.gtimg.com/newsapp_bt/0/10701537095/1000',
+      background: "rgba(247,251,252,1)",
+      guideCardHeight: 40,
+      operateCardHeight: 145,
+      history: true,
+      historySize: 60,
+      navHeight: 0,
+      robotHeader: 'https://res.wx.qq.com/mmspraiweb_node/dist/static/miniprogrampageImages/talk/leftHeader.png',
+      userHeader: 'https://res.wx.qq.com/mmspraiweb_node/dist/static/miniprogrampageImages/talk/rightHeader.png',
+      userName: ''
+    });
+  }
+})
